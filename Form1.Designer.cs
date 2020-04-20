6412ee13fc6889538370c49511a4b1d0d9e6558d
Leonardo <leonardoandre320@gmail.com>
2020-04-19 22:25:55 -0300

    Algumas programacoes visuais mesmo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,14 +35,33 @@
             this.bntFechar = new System.Windows.Forms.PictureBox();
             this.ImagensBotoes = new System.Windows.Forms.ImageList(this.components);
             this.bntAtivado = new System.Windows.Forms.PictureBox();
-            this.lblCaminho = new System.Windows.Forms.Label();
-            this.Nome = new System.Windows.Forms.Label();
             this.grupoConfig = new System.Windows.Forms.GroupBox();
+            this.txtLocal = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.mais = new System.Windows.Forms.Label();
+            this.lblLocal = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.TelaTamanho = new System.Windows.Forms.PictureBox();
+            this.lblSuperior = new System.Windows.Forms.Label();
+            this.SalvarArquivo = new System.Windows.Forms.SaveFileDialog();
+            this.ListaFormatos = new System.Windows.Forms.ListBox();
+            this.lblFormato = new System.Windows.Forms.Label();
+            this.UltimaImagem = new System.Windows.Forms.PictureBox();
+            this.lblUltimaImagem = new System.Windows.Forms.Label();
+            this.lblSaida = new System.Windows.Forms.Label();
+            this.Temporizador = new System.Windows.Forms.Timer(this.components);
+            this.lblInferior = new System.Windows.Forms.Label();
+            this.lblEsquerda = new System.Windows.Forms.Label();
+            this.lblDireita = new System.Windows.Forms.Label();
             this.Barra.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bntMinimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bntFechar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bntAtivado)).BeginInit();
             this.grupoConfig.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TelaTamanho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UltimaImagem)).BeginInit();
             this.SuspendLayout();
             // 
             // Titulo
@@ -98,52 +117,215 @@
             // bntAtivado
             // 
             this.bntAtivado.BackColor = System.Drawing.Color.Lime;
-            this.bntAtivado.Location = new System.Drawing.Point(13, 37);
+            this.bntAtivado.Location = new System.Drawing.Point(461, 214);
             this.bntAtivado.Name = "bntAtivado";
             this.bntAtivado.Size = new System.Drawing.Size(100, 20);
             this.bntAtivado.TabIndex = 2;
             this.bntAtivado.TabStop = false;
             // 
-            // lblCaminho
-            // 
-            this.lblCaminho.AutoSize = true;
-            this.lblCaminho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
-            this.lblCaminho.ForeColor = System.Drawing.Color.White;
-            this.lblCaminho.Location = new System.Drawing.Point(6, 16);
-            this.lblCaminho.Name = "lblCaminho";
-            this.lblCaminho.Size = new System.Drawing.Size(48, 13);
-            this.lblCaminho.TabIndex = 3;
-            this.lblCaminho.Text = "Caminho";
-            // 
-            // Nome
-            // 
-            this.Nome.AutoSize = true;
-            this.Nome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
-            this.Nome.ForeColor = System.Drawing.Color.White;
-            this.Nome.Location = new System.Drawing.Point(6, 40);
-            this.Nome.Name = "Nome";
-            this.Nome.Size = new System.Drawing.Size(35, 13);
-            this.Nome.TabIndex = 4;
-            this.Nome.Text = "Nome";
-            // 
             // grupoConfig
             // 
-            this.grupoConfig.Controls.Add(this.lblCaminho);
-            this.grupoConfig.Controls.Add(this.Nome);
+            this.grupoConfig.Controls.Add(this.lblDireita);
+            this.grupoConfig.Controls.Add(this.lblEsquerda);
+            this.grupoConfig.Controls.Add(this.lblInferior);
+            this.grupoConfig.Controls.Add(this.lblUltimaImagem);
+            this.grupoConfig.Controls.Add(this.UltimaImagem);
+            this.grupoConfig.Controls.Add(this.lblFormato);
+            this.grupoConfig.Controls.Add(this.ListaFormatos);
+            this.grupoConfig.Controls.Add(this.lblSuperior);
+            this.grupoConfig.Controls.Add(this.TelaTamanho);
+            this.grupoConfig.Controls.Add(this.lblBuscar);
+            this.grupoConfig.Controls.Add(this.lblNome);
+            this.grupoConfig.Controls.Add(this.lblLocal);
+            this.grupoConfig.Controls.Add(this.mais);
+            this.grupoConfig.Controls.Add(this.txtNumero);
+            this.grupoConfig.Controls.Add(this.txtNome);
+            this.grupoConfig.Controls.Add(this.txtLocal);
             this.grupoConfig.ForeColor = System.Drawing.Color.White;
-            this.grupoConfig.Location = new System.Drawing.Point(12, 63);
+            this.grupoConfig.Location = new System.Drawing.Point(12, 36);
             this.grupoConfig.Name = "grupoConfig";
-            this.grupoConfig.Size = new System.Drawing.Size(200, 186);
+            this.grupoConfig.Size = new System.Drawing.Size(549, 172);
             this.grupoConfig.TabIndex = 5;
             this.grupoConfig.TabStop = false;
             this.grupoConfig.Text = "Configuraçoes";
+            // 
+            // txtLocal
+            // 
+            this.txtLocal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.txtLocal.ForeColor = System.Drawing.Color.White;
+            this.txtLocal.Location = new System.Drawing.Point(48, 20);
+            this.txtLocal.Name = "txtLocal";
+            this.txtLocal.Size = new System.Drawing.Size(468, 20);
+            this.txtLocal.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.txtNome.ForeColor = System.Drawing.Color.White;
+            this.txtNome.Location = new System.Drawing.Point(48, 46);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(416, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // mais
+            // 
+            this.mais.AutoSize = true;
+            this.mais.Location = new System.Drawing.Point(470, 49);
+            this.mais.Name = "mais";
+            this.mais.Size = new System.Drawing.Size(13, 13);
+            this.mais.TabIndex = 2;
+            this.mais.Text = "+";
+            // 
+            // lblLocal
+            // 
+            this.lblLocal.AutoSize = true;
+            this.lblLocal.Location = new System.Drawing.Point(6, 23);
+            this.lblLocal.Name = "lblLocal";
+            this.lblLocal.Size = new System.Drawing.Size(36, 13);
+            this.lblLocal.TabIndex = 3;
+            this.lblLocal.Text = "Local:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(6, 49);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 4;
+            this.lblNome.Text = "Nome:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.txtNumero.ForeColor = System.Drawing.Color.White;
+            this.txtNumero.Location = new System.Drawing.Point(489, 46);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(54, 20);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.lblBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscar.Location = new System.Drawing.Point(522, 20);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(21, 20);
+            this.lblBuscar.TabIndex = 5;
+            this.lblBuscar.Text = "...";
+            this.lblBuscar.Click += new System.EventHandler(this.lblBuscar_Click);
+            this.lblBuscar.MouseEnter += new System.EventHandler(this.SelecionarBuscar);
+            this.lblBuscar.MouseLeave += new System.EventHandler(this.DesSelecionarBuscar);
+            // 
+            // TelaTamanho
+            // 
+            this.TelaTamanho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.TelaTamanho.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TelaTamanho.Location = new System.Drawing.Point(233, 99);
+            this.TelaTamanho.Name = "TelaTamanho";
+            this.TelaTamanho.Size = new System.Drawing.Size(100, 50);
+            this.TelaTamanho.TabIndex = 6;
+            this.TelaTamanho.TabStop = false;
+            // 
+            // lblSuperior
+            // 
+            this.lblSuperior.AutoSize = true;
+            this.lblSuperior.Location = new System.Drawing.Point(230, 83);
+            this.lblSuperior.Name = "lblSuperior";
+            this.lblSuperior.Size = new System.Drawing.Size(24, 13);
+            this.lblSuperior.TabIndex = 7;
+            this.lblSuperior.Text = "0px";
+            // 
+            // ListaFormatos
+            // 
+            this.ListaFormatos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ListaFormatos.ForeColor = System.Drawing.Color.White;
+            this.ListaFormatos.FormattingEnabled = true;
+            this.ListaFormatos.Items.AddRange(new object[] {
+            "BMP",
+            "ICON",
+            "JPEG",
+            "PNG"});
+            this.ListaFormatos.Location = new System.Drawing.Point(9, 99);
+            this.ListaFormatos.Name = "ListaFormatos";
+            this.ListaFormatos.Size = new System.Drawing.Size(120, 56);
+            this.ListaFormatos.TabIndex = 3;
+            // 
+            // lblFormato
+            // 
+            this.lblFormato.AutoSize = true;
+            this.lblFormato.Location = new System.Drawing.Point(6, 83);
+            this.lblFormato.Name = "lblFormato";
+            this.lblFormato.Size = new System.Drawing.Size(48, 13);
+            this.lblFormato.TabIndex = 10;
+            this.lblFormato.Text = "Formato:";
+            // 
+            // UltimaImagem
+            // 
+            this.UltimaImagem.Location = new System.Drawing.Point(447, 101);
+            this.UltimaImagem.Name = "UltimaImagem";
+            this.UltimaImagem.Size = new System.Drawing.Size(96, 54);
+            this.UltimaImagem.TabIndex = 11;
+            this.UltimaImagem.TabStop = false;
+            // 
+            // lblUltimaImagem
+            // 
+            this.lblUltimaImagem.AutoSize = true;
+            this.lblUltimaImagem.Location = new System.Drawing.Point(446, 83);
+            this.lblUltimaImagem.Name = "lblUltimaImagem";
+            this.lblUltimaImagem.Size = new System.Drawing.Size(78, 13);
+            this.lblUltimaImagem.TabIndex = 12;
+            this.lblUltimaImagem.Text = "Ultima imagem:";
+            // 
+            // lblSaida
+            // 
+            this.lblSaida.AutoSize = true;
+            this.lblSaida.ForeColor = System.Drawing.Color.White;
+            this.lblSaida.Location = new System.Drawing.Point(12, 211);
+            this.lblSaida.Name = "lblSaida";
+            this.lblSaida.Size = new System.Drawing.Size(37, 13);
+            this.lblSaida.TabIndex = 6;
+            this.lblSaida.Text = "Saida:";
+            // 
+            // Temporizador
+            // 
+            this.Temporizador.Enabled = true;
+            this.Temporizador.Tick += new System.EventHandler(this.Temporizador_Tick);
+            // 
+            // lblInferior
+            // 
+            this.lblInferior.AutoSize = true;
+            this.lblInferior.Location = new System.Drawing.Point(230, 152);
+            this.lblInferior.Name = "lblInferior";
+            this.lblInferior.Size = new System.Drawing.Size(24, 13);
+            this.lblInferior.TabIndex = 13;
+            this.lblInferior.Text = "0px";
+            // 
+            // lblEsquerda
+            // 
+            this.lblEsquerda.AutoSize = true;
+            this.lblEsquerda.Location = new System.Drawing.Point(203, 101);
+            this.lblEsquerda.Name = "lblEsquerda";
+            this.lblEsquerda.Size = new System.Drawing.Size(42, 13);
+            this.lblEsquerda.TabIndex = 14;
+            this.lblEsquerda.Text = "1000px";
+            // 
+            // lblDireita
+            // 
+            this.lblDireita.AutoSize = true;
+            this.lblDireita.Location = new System.Drawing.Point(339, 101);
+            this.lblDireita.Name = "lblDireita";
+            this.lblDireita.Size = new System.Drawing.Size(24, 13);
+            this.lblDireita.TabIndex = 15;
+            this.lblDireita.Text = "0px";
             // 
             // TelaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(573, 261);
+            this.ClientSize = new System.Drawing.Size(573, 242);
+            this.Controls.Add(this.lblSaida);
             this.Controls.Add(this.grupoConfig);
             this.Controls.Add(this.bntAtivado);
             this.Controls.Add(this.Barra);
@@ -158,7 +340,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.bntAtivado)).EndInit();
             this.grupoConfig.ResumeLayout(false);
             this.grupoConfig.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TelaTamanho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UltimaImagem)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -170,9 +355,26 @@
         private System.Windows.Forms.PictureBox bntFechar;
         private System.Windows.Forms.ImageList ImagensBotoes;
         private System.Windows.Forms.PictureBox bntAtivado;
-        private System.Windows.Forms.Label lblCaminho;
-        private System.Windows.Forms.Label Nome;
         private System.Windows.Forms.GroupBox grupoConfig;
+        private System.Windows.Forms.TextBox txtLocal;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label mais;
+        private System.Windows.Forms.Label lblLocal;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.PictureBox TelaTamanho;
+        private System.Windows.Forms.Label lblSuperior;
+        private System.Windows.Forms.SaveFileDialog SalvarArquivo;
+        private System.Windows.Forms.ListBox ListaFormatos;
+        private System.Windows.Forms.Label lblFormato;
+        private System.Windows.Forms.PictureBox UltimaImagem;
+        private System.Windows.Forms.Label lblUltimaImagem;
+        private System.Windows.Forms.Label lblSaida;
+        private System.Windows.Forms.Timer Temporizador;
+        private System.Windows.Forms.Label lblInferior;
+        private System.Windows.Forms.Label lblEsquerda;
+        private System.Windows.Forms.Label lblDireita;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace Screen2._0
 {
@@ -20,12 +22,15 @@ namespace Screen2._0
 
             InitializeComponent();
 
+            ListaFormatos.SelectedIndex = 3;//Seleciona Png
             Barra.MouseDown += new MouseEventHandler(MouseBaixo);
             Barra.MouseMove += new MouseEventHandler(MovimentoMouse);
 
             Titulo.MouseDown += new MouseEventHandler(MouseBaixo);
             Titulo.MouseMove += new MouseEventHandler(MovimentoMouse);
         }
+
+        #region Movimentacao da janela
         private void MouseBaixo(object sender,MouseEventArgs e)
         {
             if (e.Button != MouseButtons.Left) return;
@@ -38,7 +43,9 @@ namespace Screen2._0
             this.Left = X + MousePosition.X;
             this.Top = Y + MousePosition.Y;
         }
+        #endregion
 
+        #region Controle Janela
         private void bntFechar_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -46,6 +53,47 @@ namespace Screen2._0
         private void bntMinimizar_Click(object sender, EventArgs e)
         {
             WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+
+        #region Buscar
+        private void SelecionarBuscar(object sender,EventArgs e)
+        {
+            lblBuscar.BackColor = Color.FromArgb(100,100,100);
+        }
+        private void DesSelecionarBuscar(object sende,EventArgs e)
+        {
+            lblBuscar.BackColor = Color.FromArgb(80,80,80);
+        }
+        private void lblBuscar_Click(object sender, EventArgs e)
+        {
+            //Coloca o nome e local se existir
+            if(txtNome.Text == "")
+            {
+                SalvarArquivo.FileName = "Imagem";
+            }
+            else
+            {
+                SalvarArquivo.FileName = txtNome.Text;
+            }
+
+            SalvarArquivo.Title = "Escolha aonde vai salvar sua imagem";
+            SalvarArquivo.ShowDialog();
+        }
+        #endregion
+
+        private void Temporizador_Tick(object sender, EventArgs e)
+        {
+            #region Mantem as labels de tamanho no centro
+            TelaTamanho.Location = new Point(Size.Width / 2 - TelaTamanho.Size.Width / 2,TelaTamanho.Location.Y);
+
+            //                               distancia da tela          tamanho da tela / 2        tamanho label / 2           altura normal
+            lblSuperior.Location = new Point(TelaTamanho.Location.X + TelaTamanho.Size.Width / 2 - lblSuperior.Size.Width / 2, lblSuperior.Location.Y);
+            lblInferior.Location = new Point(TelaTamanho.Location.X + TelaTamanho.Size.Width / 2 - lblInferior.Size.Width / 2, lblInferior.Location.Y);
+
+            lblEsquerda.Location = new Point(lblEsquerda.Location.X, TelaTamanho.Location.Y + TelaTamanho.Size.Height / 2 - lblEsquerda.Size.Width / 2);
+
+            #endregion
         }
     }
 }
